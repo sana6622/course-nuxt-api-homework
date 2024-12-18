72384acbed65f961c627dedaa7b72fe26f563171
Yetta <yetta@supergeo.com.tw>
2024-12-18 17:44:46 +0800

    login & singup page , 處理 document is not defined 問題

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["@/assets/styles/all.scss", "bootstrap/dist/css/bootstrap.min.css"],
-  vite: {
-    define: {
-      "process.env.DEBUG": false, // 解決 Bootstrap 的環境變數警告
-    },
-    // css: {
-    //   preprocessorOptions: {
-    //     scss: {
-    //       additionalData: `
-    //         @use "@/assets/styles/_variables.scss"; // 在全部的元件都引入 _variables.scss
-    //       `,
-    //     },
-    //   },
-    // },
-  },
+  plugins: [{ src: "~/plugins/bootstrap.client.js", ssr: false }], // 修正語法,
 });
